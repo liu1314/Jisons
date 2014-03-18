8bc83dbc77f5be7a13df13fd0a96c8e7d76e810f
jiailiuyan <366006820@qq.com>
2014-03-18 14:14:00 +0800

    增加 对于visual的查询 对于IEnumerable的扩展

---
+++ JiHelper/Helpers/JisonsFindVisual.cs
@@ -0,0 +1,136 @@
+﻿/* 
+ * 
+ * FileName:   JisonsFindVisual.cs
+ * Version:    1.0
+ * Date:       2014.03.18
+ * Author:     Ji
+ * 
+ *========================================
+ * @namespace  Jisons 
+ * @class      JisonsFindVisual
+ * @extends    
+ * 
+ *             对于Visual 元素的查询方法扩展
+ * 
+ *========================================
+ * 
+ * Copyright © 迹I柳燕
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Jisons
+{
+    public static class JisonsFindVisual
+    {
+
+        /// <summary> 向上查找指定类型的逻辑容器 </summary>
+        /// <typeparam name="T">欲查找的控件类型</typeparam>
+        /// <param name="descendent">查找的子容器</param>
+        /// <param name="isContainsSelf">查找是否包含自身</param>
+        /// <returns>返回第一个查找到的父控件 T?</returns>
+        public static T FindVisualParent<T>(this DependencyObject descendent, bool isContainsSelf = true) where T : DependencyObject
+        {
+            T ancestor = null;
+            DependencyObject scan = isContainsSelf ? descendent : VisualTreeHelper.GetParent(descendent);
+            ancestor = null;
+            while (scan != null && ((ancestor = scan as T) == null))
+            {
+                scan = VisualTreeHelper.GetParent(scan);
+            }
+            return ancestor;
+        }
+
+        /// <summary> 查找指定类型的子物体 </summary>
+        /// <typeparam name="T">欲查找的控件类型</typeparam>
+        /// <param name="descendent">查找的根容器</param>
+        /// <param name="isContainsSelf">查找是否包含自身</param>
+        /// <returns>返回第一个查找到的子控件 T?</returns>
+        public static T FindVisualChild<T>(this DependencyObject descendent, bool isContainsSelf = true) where T : DependencyObject
+        {
+            T found = null;
+            if (descendent is T && isContainsSelf)
+            {
+                found = descendent as T;
+            }
+            else
+            {
+                var childlen = VisualTreeHelper.GetChildrenCount(descendent);
+                for (int i = 0; i < childlen; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(descendent, i);
+                    var target = child as T;
+                    if (target == null)
+                    {
+                        found = FindVisualChild<T>(child, false);
+                        if (found != null)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        found = (T)child;
+                        break;
+                    }
+                }
+            }
+            return found;
+        }
+
+        /// <summary> 查找指定类型的子物体 </summary>
+        /// <typeparam name="T">欲查找的控件类型</typeparam>
+        /// <param name="descendent">查找的根容器</param>
+        /// <param name="isContainsSelf">查找是否包含自身</param>
+        /// <returns>返回所有查找到的子控件 T?</returns>
+        public static IEnumerable<T> FindAllVisualChild<T>(this DependencyObject descendent, bool isContainsSelf = true) where T : DependencyObject
+        {
+            T found = null;
+            if (descendent is T && isContainsSelf)
+            {
+                found = descendent as T;
+            }
+            else
+            {
+                var childlen = VisualTreeHelper.GetChildrenCount(descendent);
+                for (int i = 0; i < childlen; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(descendent, i);
+                    var target = child as T;
+                    if (target == null)
+                    {
+                        FindAllVisualChild<T>(child, false);
+                    }
+                    else
+                    {
+                        yield return (T)child;
+                    }
+                }
+            }
+            yield return found;
+        }
+
+        /// <summary> 在列表控件中中查询子元素 </summary>
+        /// <typeparam name="T">欲查找的控件类型</typeparam>
+        /// <param name="selector">元数据列表</param>
+        /// <param name="data">控件的数据源</param>
+        /// <returns>返回查找到的类型 T?</returns>
+        public static T FindControlInSelector<T>(this Selector selector, object data) where T : DependencyObject
+        {
+            var item = selector.ItemContainerGenerator.ContainerFromItem(data);
+            if (item != null)
+            {
+                return item.FindVisualChild<T>();
+            }
+            return default(T);
+        }
+
+    }
+}
---
+++ JiHelper/Helpers/JisonsIEnumerable.cs
@@ -0,0 +1,45 @@
+﻿/* 
+ * 
+ * FileName:   JisonsIEnumerable.cs
+ * Version:    1.0
+ * Date:       2014.03.18
+ * Author:     Ji
+ * 
+ *========================================
+ * @namespace  Jisons 
+ * @class      JisonsIEnumerable
+ * @extends    
+ * 
+ *             对于IEnumerable 的Linq扩展查询
+ * 
+ *========================================
+ * 
+ * Copyright © 迹I柳燕
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jisons
+{
+    public static class JisonsIEnumerable
+    {
+
+        /// <summary>
+        /// 具有锁定检查的遍历执行函数体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">欲执行循环的列表,继承IEnumerable</param>
+        /// <param name="action">执行循环的函数体</param>
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action.Invoke(item);
+            }
+        }
+    }
+}
