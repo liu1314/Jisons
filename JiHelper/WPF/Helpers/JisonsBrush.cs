690a4649c0936e689f30909b457ad58f9e35b92c
jiailiuyan <366006820@qq.com>
2014-03-18 17:38:49 +0800

    增加 GetRandomBrush

--- JiHelper/WPF/Helpers/JisonsBrush.cs
+++ JiHelper/WPF/Helpers/JisonsBrush.cs
@@ -21,6 +21,7 @@
  * 
  */
 
+using System;
 using System.Windows.Media;
 
 namespace Jisons
@@ -31,10 +32,39 @@ namespace Jisons
         /// <summary> 十六进制颜色值转换为Brush </summary>
         /// <param name="brushStr">传入的颜色字符串</param>
         /// <returns>Brush</returns>
-        public static Brush StringConvertToBrush(this string brushStr)
+        public static Brush ConvertToBrush(this string brushStr)
         {
             return new SolidColorBrush((Color)ColorConverter.ConvertFromString(brushStr));
         }
+
+        static Random random = new Random();
+        /// <summary> 随机生成Brush </summary>
+        /// <param name="isTransparent">是否可透明</param>
+        /// <returns>生成的Brush</returns>
+        public static Brush GetRandomBrush(bool isTransparent = false)
+        {
+            var r = Convert.ToString(random.Next(1, 150), 16);
+            r = r.Length < 2 ? r + "F" : r;
+
+            var g = Convert.ToString(random.Next(1, 150), 16);
+            g = g.Length < 2 ? g + "F" : g;
+
+            var b = Convert.ToString(random.Next(1, 150), 16);
+            b = b.Length < 2 ? b + "F" : b;
+
+            if (isTransparent)
+            {
+                var f = Convert.ToString(random.Next(1, 150), 16);
+                f = f.Length < 2 ? b + "F" : b;
+
+                return ("#" + f + r + g + b).ConvertToBrush();
+            }
+            else
+            {
+                return ("#FF" + r + g + b).ConvertToBrush();
+            }
+        }
+
 
     }
 }
